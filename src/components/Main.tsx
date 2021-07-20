b264e3f16544045c99eb3a4dd42b103956cdc65f
dawidrydzewski11elk <dawidrydzewski11elk@gmail.com>
2021-07-21 00:08:06 +0200

    Add Contents in Main container

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,11 +1,13 @@
 import React from "react";
+import Contents from "./Contents/Contents";
 import { MainContainer } from "./style";
 import TopNavbar from "./TopNavbar/TopNavbar";
 
 const Main = () => {
   return (
     <MainContainer>
-      <TopNavbar/>
+      <TopNavbar />
+      <Contents />
     </MainContainer>
   );
 };
